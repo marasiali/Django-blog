b7e7b5387a7dd84e7843adaf7ef22f3f1cb4eaf2
Ali Marasizadeh <marasiali@gmail.com>
2020-07-24 14:22:15 +0430

    create Article model, add to admin panel and add some options for it. and set media_rool & media_url

--- myproj/blog/admin.py
+++ myproj/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleClass(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'status')
+    list_filter = ('published', 'status')
+    search_fields = ('title', 'description', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['status', '-published']
+
+admin.site.register(Article, ArticleClass)
