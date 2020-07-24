7ce542355a002690d98445751851600ca1f9e07a
Ali Marasizadeh <marasiali@gmail.com>
2020-07-24 10:00:36 +0430

    some practices on urls, views and templates(jinja syntax)

---
+++ myproj/blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index
+
+app_name = 'blog'
+urlpatterns = [
+    path('', index, name='index'),
+]
--- myproj/blog/views.py
+++ myproj/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    context = {
+        'posts' : [
+            {
+                'title' : 'First',
+                'desc' : 'I am first',
+                'number' : 1
+            },
+            {
+                'title' : 'Second',
+                'desc' : 'I am second',
+                'number' : 2
+            },
+            {
+                'title' : 'Third',
+                'desc' : 'I am third',
+                'number' : 3
+            },
+        ]
+    }
+    return render(request, 'index.html', context)
