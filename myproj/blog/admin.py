301123be2206c3fe54fdd909be3352cc3ac2d729
Ali Marasizadeh <marasiali@gmail.com>
2020-07-29 15:22:44 +0430

    Create Category model & connect it to articles & show them on admin panel

--- myproj/blog/admin.py
+++ myproj/blog/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
-from .models import Article
+from .models import Category, Article
+from .extensions.utils import convert_to_persian_digits
 
-class ArticleClass(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'jpublished', 'status')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position', 'title', 'slug', 'category_article_count', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'slug')
+    prepopulated_field = {'slug': ('title',)}
+
+    def category_article_count(self, obj):
+        cat_all_articles = Article.objects.filter(category=obj)
+        cat_draft_articles = cat_all_articles.filter(status='d')
+        output = '{}  ({} پیش‌نویس)'.format(cat_all_articles.count(),
+                                          cat_draft_articles.count())
+        return convert_to_persian_digits(output)
+    category_article_count.short_description = 'تعداد مطالب'
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'jpublished', 'category_to_str', 'status')
     list_filter = ('published', 'status')
     search_fields = ('title', 'description', 'slug')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['status', '-published']
 
-admin.site.register(Article, ArticleClass)
+    def category_to_str(self, obj):
+        return ', '.join(map(lambda cat : cat.title, obj.category.all()))
+    category_to_str.short_description = 'دسته‌بندی‌ها'
+    
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
--- myproj/blog/models.py
+++ myproj/blog/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 from .extensions.utils import convert_to_jalali
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='لینک')
+    position = models.SmallIntegerField(verbose_name='ترتیب')
+    status = models.BooleanField(default=True, verbose_name='فعال باشد؟')
+    
+    class Meta:
+        verbose_name = 'دسته‌بندی'
+        verbose_name_plural = 'دسته‌بندی‌ها'
+        ordering = ['position']
+    
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     STATUS_CHOICES = (
         ('d', 'پیش‌نویس'),
@@ -12,6 +27,7 @@ class Article(models.Model):
     slug = models.SlugField(max_length=100, unique=True, verbose_name='لینک')
     description = models.TextField(verbose_name='متن')
     thumbnail = models.ImageField(upload_to='images', verbose_name='تصویر اصلی')
+    category = models.ManyToManyField(Category, verbose_name='دسته‌بندی')
     published = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
