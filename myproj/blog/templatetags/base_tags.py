1e48c1aa64cdee7fe28e8b2644804cdf5bc757cb
Ali Marasizadeh <marasiali@gmail.com>
2020-08-01 11:25:46 +0430

    show categoories in navbar and using templatetags

---
+++ myproj/blog/templatetags/base_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from ..models import Category
+
+register = template.Library()
+
+@register.simple_tag
+def title():
+    return 'وبلاگ جنگویی'
+
+@register.simple_tag
+def brand_name():
+    return 'جنگو وب'
+
+@register.inclusion_tag('blog/partial/category_navbar.html')
+def category_navbar():
+    return {
+        'categories' : Category.objects.filter(status=True)
+    }
