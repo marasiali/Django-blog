8f93d8bc567ac1e215371a1ea8f3d8910d285f45
Ali Marasizadeh <marasiali@gmail.com>
2020-08-10 22:19:05 +0430

    add admin actions to Article

--- myproj/blog/admin.py
+++ myproj/blog/admin.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Category, Article
 from .extensions.utils import convert_to_persian_digits
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('position', 'title', 'slug', 'parent', 'category_article_count', 'status')
@@ -22,12 +23,26 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('published', 'status')
     search_fields = ('title', 'description', 'slug')
     prepopulated_fields = {'slug': ('title',)}
-    ordering = ['status', '-published']
+    ordering = ('status', '-published')
+    actions = ('make_published', 'make_draft')
 
     def category_to_str(self, obj):
         return ', '.join(map(lambda cat : cat.title, obj.category.available()))
     category_to_str.short_description = 'دسته‌بندی‌ها'
     
+    def make_published(self, request, queryset):
+            updated = queryset.update(status='p')
+            updated = convert_to_persian_digits(updated)
+            msg = f'{updated} مطلب منتشر شد.'
+            self.message_user(request, msg, messages.SUCCESS)
+    make_published.short_description = 'انتشار'
+
+    def make_draft(self, request, queryset):
+            updated = queryset.update(status='d')
+            updated = convert_to_persian_digits(updated)
+            msg = f'{updated} مطلب پیش‌نویس شد.'
+            self.message_user(request, msg, messages.SUCCESS)
+    make_draft.short_description = 'پیش‌نویس کردن'
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
