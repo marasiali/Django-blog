0420653f2a3134fb9be047d0b62808cf251b5931
Ali Marasizadeh <marasiali@gmail.com>
2020-08-25 03:51:51 +0430

    fix wrong import UserModel (not updated after create custom User model)

--- myproj/blog/views.py
+++ myproj/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, Http404
-from django.contrib.auth.models import User
+from account.models import User
 from .models import Article, Category
 
 
